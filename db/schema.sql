99c2b362a56552af9da4367b509a82235771cdc8
taqwa <trushdan@gmail.com>
2018-03-12 21:28:13 -0400

    deleted schema comment.

--- db/schema.sql
+++ db/schema.sql
@@ -1,12 +1,3 @@
--- Write SQL queries this file that do the following:
---
---    * Create the `things_db`.
---    * Switch to or use the `things_db`.
---    * Create a `books` table with these fields:
---      * **id**: an auto incrementing int that serves as the primary key.
---      * **burger_name**: a string.
---      * **devoured**: a boolean.
-
 CREATE DATABASE things_db;
 USE things_db;
 
@@ -17,4 +8,3 @@ CREATE TABLE books(
   read_status BOOLEAN DEFAULT false,
   PRIMARY KEY (id)
 );
- 
